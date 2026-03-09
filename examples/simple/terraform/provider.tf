2170b52eca03063ac09dccba8d90f3909529341e
Hen Itzhaki <hen.itzhaki2@zesty.co>
2026-03-09 17:28:06 +0200

    fix: update helm provider configuration to use assignment syntax for kubernetes and exec blocks

--- examples/simple/terraform/provider.tf
+++ examples/simple/terraform/provider.tf
@@ -14,10 +14,10 @@ data "aws_eks_cluster" "cluster" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
